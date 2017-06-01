f13922afa9d072693276a2ce0caa6099ef70a0cb
eef2118 <esoto@lavca.org>
2017-06-01 18:22:57 -0400

    Commit
    Commiting

--- process-form.php
+++ process-form.php
@@ -7,13 +7,13 @@
 */
 
 $to = "office@example.com";
-$subject = "Signup from Ventcamp";
+$subject = "Thank you for subscribing to the LatAm Venture Bulletin!";
 $newsletter = array(true => 'Yes!', 0 => 'No please');
 $message = "Ticket type: " . $_POST['ticket'];
-$message .= "<br/>Fullname: " . $_POST['fullname'];
-$message .= "<br>Company: " . $_POST['company'];
-$message .= "<br>Email: " . $_POST['email'];
-$message .= "<br>Newsletter subscription: " . $newsletter[$_POST['newsletter']];
+$message .= "<br/>Fullname: " . $_POST['Elisabeth Soto'];
+$message .= "<br>Company: " . $_POST['LAVCA'];
+$message .= "<br>Email: " . $_POST['esoto@lavca.org'];
+$message .= "<br>Newsletter subscription: " . $newsletter[$_POST['Thank you for subscribing to the LatAm Venture Bulletin!']];
 
 $headers  = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type: text/html; charset=utf-8" . "\r\n";
